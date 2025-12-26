e13c946915f104d41b186b7d2b39243372176c28
Prudhvi Godithi <pgodithi@amazon.com>
2025-12-26 14:08:25 -0800

    Ingtra segment
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- lucene/core/src/java/org/apache/lucene/search/PointRangeQuery.java
+++ lucene/core/src/java/org/apache/lucene/search/PointRangeQuery.java
@@ -457,52 +457,56 @@ public abstract class PointRangeQuery extends Query {
       @Override
       public ScorerSupplier scorerSupplier(IndexSearcher.LeafReaderContextPartition partition)
           throws IOException {
-        if (partition.numPartitions <= 1) {
-          return scorerSupplier(partition.ctx);
-        }
-
-        DocIdSet docIdSet = cachedDocIdSet;
-        if (docIdSet == null) {
-          synchronized (cacheLock) {
-            docIdSet = cachedDocIdSet;
-            if (docIdSet == null) {
-              ScorerSupplier supplier = scorerSupplier(partition.ctx);
-              if (supplier == null) {
-                cachedDocIdSet = DocIdSet.EMPTY;
-                return null;
-              }
-              DocIdSetIterator iter = supplier.get(Long.MAX_VALUE).iterator();
-              if (iter instanceof BitSetIterator bsi) {
-                cachedDocIdSet = new BitDocIdSet(bsi.getBitSet());
-              } else {
-                FixedBitSet bits = new FixedBitSet(partition.ctx.reader().maxDoc());
-                int doc;
-                while ((doc = iter.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
-                  bits.set(doc);
-                }
-                cachedDocIdSet = new BitDocIdSet(bits);
-              }
+        // For partitioned search, share the BKD traversal result across partitions
+        if (partition.numPartitions > 1) {
+          DocIdSet docIdSet = cachedDocIdSet;
+          if (docIdSet == null) {
+            synchronized (cacheLock) {
               docIdSet = cachedDocIdSet;
+              if (docIdSet == null) {
+                // Build once
+                ScorerSupplier supplier = scorerSupplier(partition.ctx);
+                if (supplier == null) {
+                  cachedDocIdSet = DocIdSet.EMPTY;
+                  return null;
+                }
+                // Get the iterator to materialize the bitset
+                DocIdSetIterator iter = supplier.get(Long.MAX_VALUE).iterator();
+                if (iter instanceof BitSetIterator bsi) {
+                  cachedDocIdSet = new BitDocIdSet(bsi.getBitSet());
+                } else {
+                  // Materialize to FixedBitSet
+                  FixedBitSet bits = new FixedBitSet(partition.ctx.reader().maxDoc());
+                  int doc;
+                  while ((doc = iter.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
+                    bits.set(doc);
+                  }
+                  cachedDocIdSet = new BitDocIdSet(bits);
+                }
+                docIdSet = cachedDocIdSet;
+              }
             }
           }
-        }
-
-        if (docIdSet == DocIdSet.EMPTY) {
-          return null;
-        }
-
-        final DocIdSet finalDocIdSet = docIdSet;
-        return new ScorerSupplier() {
-          @Override
-          public Scorer get(long leadCost) throws IOException {
-            return new ConstantScoreScorer(score(), scoreMode, finalDocIdSet.iterator());
+          
+          if (docIdSet == DocIdSet.EMPTY) {
+            return null;
           }
+          
+          final DocIdSet finalDocIdSet = docIdSet;
+          return new ScorerSupplier() {
+            @Override
+            public Scorer get(long leadCost) throws IOException {
+              return new ConstantScoreScorer(score(), scoreMode, finalDocIdSet.iterator());
+            }
 
-          @Override
-          public long cost() {
-            return finalDocIdSet.ramBytesUsed();
-          }
-        };
+            @Override
+            public long cost() {
+              return finalDocIdSet.ramBytesUsed();
+            }
+          };
+        }
+        
+        return scorerSupplier(partition.ctx);
       }
     };
   }
